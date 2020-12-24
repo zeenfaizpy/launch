60d4b548753e36932260561776d66e1e11144c0d
Mohammad Faizal <zeenfaiz@gmail.com>
2020-12-24 08:16:38 +0400

    update launch time

--- src/components/countDown/index.js
+++ src/components/countDown/index.js
@@ -21,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
 
 const CountDown = () => {
     const { heading } = useStyles();
-    const launchTime = "2020-12-30 00:00:00";
+    const launchTime = "2021-01-14 00:00:00";
     return (
         <section id="countdown" className={heading}>
             <Timer launchTime={launchTime} />
